47e148d2abb2a2e66a295b02a8d8586c138d30e3
Flor59165 <florianp59580@gmail.com>
2025-03-28 00:18:04 +0100

    Config

---
+++ src/main/java/me/pushout/Config.java
@@ -0,0 +1,8 @@
+package me.pushout;
+
+public class Config {
+    public static double KB_MULTIPLIER = 16.0; // multiplicateur dans la formule (exemple dans KOManager)
+    public static double KB_BASE = 0.5;         // valeur de base (exemple dans KOManager)
+    public static boolean JUMP_BOOST_ENABLED = true;
+    public static double HIT_PERCENT_GAIN = 2.3;
+}
